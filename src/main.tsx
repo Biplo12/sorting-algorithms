0a02e7ff63e76638bb7e5b90129be731afe4cbf1
Robert Siński <75808735+Biplo12@users.noreply.github.com>
2023-06-17 21:51:47 +0200

    feat: build fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/main.css";
-import Layout from "./components/layout/layout";
+import Layout from "./components/layout/Layout";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>

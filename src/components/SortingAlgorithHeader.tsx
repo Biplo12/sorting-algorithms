2971fd7daaa122c13c55973e445180e1a9e80780
Robert Siński <75808735+Biplo12@users.noreply.github.com>
2023-08-25 13:43:46 +0200

    feat: header change

--- src/components/SortingAlgorithHeader.tsx
+++ src/components/SortingAlgorithHeader.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+
+const GITHUB_URL = "https://www.github.com/biplo12";
+
 const SortingAlgorithHeader: React.FC = (): JSX.Element => {
   return (
     <div className="flex flex-col justify-center items-center gap-5 text-white text-center px-4">
@@ -8,7 +11,21 @@ const SortingAlgorithHeader: React.FC = (): JSX.Element => {
         </span>
       </h1>
       <p>
-        <span className="text-lg">Click on an algorithm to sort the array</span>
+        <span className="text-lg">
+          Click on an algorithm to sort the array
+          <br />
+        </span>
+        <span className="text-lg">
+          Made by:{" "}
+          <a
+            href={GITHUB_URL}
+            className="text-[#42db1f]"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Biplo12
+          </a>
+        </span>
       </p>
     </div>
   );
